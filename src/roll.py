f41787a8f7ba06732a6e59b686081efc4d0c1a34
LloydDM <d.m.lloyd.86@gmail.com>
2017-10-18 20:37:25 -0700

    Adding Comments

--- src/roll.py
+++ src/roll.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 
-from random import randint
+from random import randint          # Standard Library modules
 
 def dice_roll(amount, modifier):
+    # Turn the die roll input into a list of integers.  Element 0 is number of dice to roll, element 1 is the number of faces on the dice.
     params = map(int, amount.split('d'))
     count = 0
+    
+    # The for loop executes and 'rolls' a die, then adds that result to count, then prints a summary of what happened
     for i in range(1, params[0] + 1):
         roll = randint(1, params[1])
         count += roll
@@ -12,7 +15,7 @@ def dice_roll(amount, modifier):
         print "Sum:    ", count
     print "Dice + Modifier:", count + modifier
 
-toss = raw_input('die roll> ')
+toss = raw_input('die roll> ').lower()
 mod = int(raw_input('modifier> '))
 
 dice_roll(toss, mod)
